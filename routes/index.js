2e73fbde841b637a597294897810e66ba9509031
Akash Shekara <akashshekara1212@gmail.com>
2016-05-21 16:52:05 -0700

    something

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	console.log(req.body.message);
-  	res.render('index');
+  	res.send("message received");
 });
 
 module.exports = router;
