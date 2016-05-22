0219438eeb96fdcae3082f6d468691de81febed5
Akash Shekara <akashshekara1212@gmail.com>
2016-05-21 18:36:47 -0700

    hey

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var wit = require('node-wit');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	console.log(req.query.message);
-  	res.send(req.query.message);
+  	res.send(JSON.stringify(req.query.message));
   	// res.render("index",{"title": req.body.message})
 });
 
