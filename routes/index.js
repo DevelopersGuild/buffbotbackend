a69772665729eb703606303b7c024504de856f67
Akash Shekara <akashshekara1212@gmail.com>
2016-05-21 19:32:22 -0700

    adding wit

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,35 @@
 var express = require('express');
 var router = express.Router();
-var wit = require('node-wit');
+const Wit = require('node-wit').Wit;
+const actions = {
+  say(sessionId, context, message, cb) {
+    console.log(message);
+    cb();
+  },
+  merge(sessionId, context, entities, message, cb) {
+    cb(context);
+  },
+  error(sessionId, context, error) {
+    console.log(error.message);
+  },
+};
+const context = {};
+const client = new Wit("NCBWVKHN3ZEVOFCFAG5CFUA5MKZB6VNO", actions);
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	console.log(req.query.message);
-  	res.send(JSON.stringify(req.query.message));
+	const context = {};
+	client.message(req.query.message, context, (error, data) => {
+	  if (error) {
+	    console.log('Oops! Got an error: ' + error);
+	  } else {
+	    console.log('Yay, got Wit.ai response: ' + JSON.stringify(data));
+	    res.send(data)
+	  }
+	});
+  	// res.send(req.query.message);
   	// res.render("index",{"title": req.body.message})
 });
 
