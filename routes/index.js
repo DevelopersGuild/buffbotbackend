1d0aec4ce4d08de5656d647e23843715a0cf84db
Akash Shekara <akashshekara1212@gmail.com>
2016-05-21 18:24:29 -0700

    changes

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	console.log(req.body.message);
-  	res.send(req.body.message);
-  	res.render("index",{"title": req.body.message})
+	console.log(req.query.message);
+  	res.send(req.query.message);
+  	// res.render("index",{"title": req.body.message})
 });
 
 module.exports = router;
